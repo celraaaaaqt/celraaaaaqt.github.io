1239e874ed45cd7a9312c425eb9b9c5a2d4c027b
celraaaaaqt <arcelzamora068@gmail.com>
2024-09-29 12:48:42 +0800

    backend.js
    hello

---
+++ backend.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  alert ("Before anything else, sana after mong mabasa 'to, hindi mawala 'yung bond and closeness natin. Kahit anong maging desisyon mo lubos kong tatanggapin! At isa pa, nakahanda ako sa mga sasabihin mo para sa'kin;).");
+}
